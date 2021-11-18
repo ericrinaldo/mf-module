f25b55673613b1b9b86b5cc24b96f33d6b765c70
ericrinaldo <52439561+ericrinaldo@users.noreply.github.com>
2021-11-18 19:37:45 +0100

    typo

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = (_, argv) => ({
       remotes: {},
       exposes: {
         '.': './src/components',
-        'components/Button': './src/components/Button/Button',
+        Button: './src/components/Button/Button',
       },
       shared: {
         ...deps,
